3ca637dcb8d6a2419f1cfda72164a5b44732c973
LaurentDardenne <ladardenne@orange.fr>
2022-10-13 13:28:37 +0200

    Encoding and comment

---
+++ src/Demos/DemoPSSA.ps1
@@ -0,0 +1,8 @@
+﻿#
+Import-Module PSScriptAnalyzer
+$Filepath=$env:Temp
+$TypesFileName="PSSA.Types.ps1xml"
+$FileName=Join-path -Path $Filepath -ChildPath $TypesFileName
+Write-Warning "Generate file '$FileName'"
+$Assembly=[System.AppDomain]::CurrentDomain.GetAssemblies()|Where-Object {$_.location -match '\.ScriptAnalyzer\.dll$'}
+$Assembly.ExportedTypes | New-ExtendedTypeData -Path $FileName -All -Force
