004bd6856fc888daaf5a31f56713352878bebc72
LaurentDardenne <ladardenne@orange.fr>
2022-10-13 12:51:44 +0200

    New demos files

---
+++ src/Demos/DemosDynamic.ps1
@@ -0,0 +1,39 @@
+﻿#Compiles an extension method and dynamically adds a scriptmethod to the targeted type
+Import-Module ExtensionMethod
+
+Add-type -TypeDefinition @'
+//https://csharp-extension.com/en/method/1002258/datetime-isfuture
+using System;
+
+public static partial class Extensions
+{
+    /// <summary>
+    ///     A DateTime extension method that query if '@this' is in the future.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>true if the value is in the future, false if not.</returns>
+    public static bool IsFuture(this DateTime @this)
+    {
+        return @this > DateTime.Now;
+    }
+}
+'@
+
+$Types=[Extensions].Assembly.ExportedTypes | Find-ExtensionMethod -ExcludeGeneric
+$ExtensionMethodInfos=$Types |
+      Get-ExtensionMethodInfo -ExcludeGeneric -ExcludeInterface |
+      New-HashTable -key "Key" -Value "Value" -MakeArray
+
+$ScriptMethods=$ExtensionMethodInfos.GetEnumerator() | New-ExtensionMethodType
+
+$Date=[System.DateTime]::now.AddDays(+1)
+
+$Parameters=@{
+ TypeName=[System.DateTime].Name
+ MemberType='ScriptMethod'
+ MemberName=$ScriptMethods.Members[0].Name
+ Value=[Scriptblock]::Create($ScriptMethods.Members[0].Script)
+}
+Update-TypeData @Parameters
+
+$Date.IsFuture()
---
+++ src/Demos/DemosWithDLL.ps1
@@ -0,0 +1,19 @@
+﻿#Loads a dll from which we extract all its extension methods.
+# !!!! This example need a specific dll.
+
+Set-Location $env:Temp
+mkdir 'TestPs1Xml'
+
+nuget install Z.ExtensionMethods
+
+$AssemblyPath="$env:Temp\Z.ExtensionMethods.2.1.1\lib\net45\Z.ExtensionMethods.dll"
+
+#One file for all types
+Add-Type -Path $AssemblyPath -Pass|
+ New-ExtendedTypeData -Path c:\temp\All.ps1xml -All
+
+#One file per type
+Add-Type -Path $AssemblyPath -Pass|
+ New-ExtendedTypeData -Path 'C:\temp\TestPs1Xml\'
+
+
