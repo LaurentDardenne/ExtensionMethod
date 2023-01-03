322b21de7550448def5acb7034b50421893b877f
Laurent Dardenne <LaDardenne@orange.fr>
2023-01-03 12:58:12 +0100

    WIP

--- tests/ExtensionMethod.Tests.ps1
+++ tests/ExtensionMethod.Tests.ps1
@@ -7,6 +7,56 @@ New-HashTable : The input object cannot be bound to any parameters for the comma
 take pipeline input or the input and its properties do not match any of the parameters that take pipeline input.
 
 
+
+---------
+
+Add-type -TypeDefinition @'
+//https://csharp-extension.com/en/method/1002258/datetime-isfuture
+using System;
+
+public static partial class Extensions
+{
+   
+    public static string Method1(this string S, bool includeBoundary=true)
+    {
+      Console.WriteLine('1');
+      return "1";
+    }
+                                                                            
+    public static string Method2(this string S, int end)
+    {
+      Console.WriteLine('2');
+      return "2";
+    }
+    public static string Method2(this string S, params object[] parameters)
+    {
+      Console.WriteLine('3');
+      return "3";
+    }
+}
+'@
+
+
+$Types=[Extensions].Assembly.ExportedTypes | Find-ExtensionMethod -ExcludeGeneric
+$ExtensionMethodInfos=$Types |
+      Get-ExtensionMethodInfo -ExcludeGeneric -ExcludeInterface |
+      New-HashTable -key "Key" -Value "Value" -MakeArray
+
+$ScriptMethods=$ExtensionMethodInfos.GetEnumerator() | New-ExtensionMethodType
+
+$Parameters=@{
+  TypeName=[System.String].Name
+  MemberType='ScriptMethod'
+  MemberName=$ScriptMethods.Members[1].Name
+  Value=[Scriptblock]::Create($ScriptMethods.Members[0].Script)
+ }
+ Update-TypeData @Parameters
+
+# $Date.IsFuture()
+
+
+-----------
+
 Describe 'Test-ClassExtensionMethod' {
     It 'Without extension method' {
         $result=Test-ClassExtensionMethod -Type System.Management.Automation.Language.TokenFlags
