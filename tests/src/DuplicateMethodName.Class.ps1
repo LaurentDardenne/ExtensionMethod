c8c99ec32ad381d879f74ce1bdef61a4fa5a79f7
Laurent Dardenne <LaDardenne@Orange.fr>
2023-01-17 13:22:32 +0100

    Adding a use case for two classes declaring the same extension method name

---
+++ tests/src/DuplicateMethodName.Class.ps1
@@ -0,0 +1,64 @@
+    $source = @"
+using System;
+
+  public static class DuplicateMethodName1
+  {
+    public static string Method1(this string S, int end)
+    {
+      return "(this string S, int end)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true)
+    {
+        return "Method1 (this string S, bool includeBoundary=true)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true,int Boundary=10)
+    {
+        return "Method1 (this string S, bool includeBoundary=true,int Boundary=10)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true,int Boundary=10,string text="test")
+    {
+        return "Method1 (this string S, bool includeBoundary=true,int Boundary=10,string text='test')";
+    }
+  }
+
+  public static class DuplicateMethodName2
+  {
+    public static string Method1(this string S, int end)
+    {
+      return "(this string S, int end)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true)
+    {
+        return "Method1 (this string S, bool includeBoundary=true)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true,int Boundary=10)
+    {
+        return "Method1 (this string S, bool includeBoundary=true,int Boundary=10)";
+    }
+
+    public static string Method1(this string S, bool includeBoundary=true,int Boundary=10,string text="test")
+    {
+        return "Method1 (this string S, bool includeBoundary=true,int Boundary=10,string text='test')";
+    }
+  }  
+"@
+
+$Assembly=Add-Type -TypeDefinition $source  -PassThru
+# $VerbosePreference='continue'
+# #ipmo G:\ps\ExtensionMethod\Release\Extensionmethod\ExtensionMethod.psd1
+# ipmo  C:\Users\1801106\Documents\projets\ExtensionMethod\Release\Extensionmethod\ExtensionMethod.psd1
+# $O=$Assembly|New-ExtendedTypeData -Path c:\temp\All.ps1xml -All
+
+<#
+M(p)
+ -> Colision !!!
+    Pour PS et/ou C#
+M(p,params)
+M(p,Object)
+
+#>
