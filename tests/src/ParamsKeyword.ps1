a49ac304547334862e650c993f2c58c7dbf294b7
Laurent Dardenne <LaDardenne@Orange.fr>
2023-02-02 11:50:49 +0100

    Add signatures

--- tests/src/ParamsKeyword.ps1
+++ tests/src/ParamsKeyword.ps1
@@ -99,12 +99,43 @@ using System;
        return "(this string S, int i, params object[] parameters)";
     }    
 
+    //Error for : public static string ArrayOfParams(this string S, int i, int j, object[] myparams)
+    // Le type 'TestOptionalAndParams5' définit déjà  un membre appelé 'ArrayOfParams' avec les mêmes types de paramètres.
+
+    //todo Collision ? 
+    public static string ArrayOfParams(this string S, int i, int j, string[] myparams)
+    {
+      Console.WriteLine("myparams :");
+      Array.ForEach(myparams, Console.WriteLine);
+      return "(this string S, int i, int j, string[] myparams)";
+   }  
+
     public static string ArrayOfParams(this string S, int i, int j, params object[] parameters)
     {
        Console.WriteLine("parameters :");
        Array.ForEach(parameters, Console.WriteLine);
        return "(this string S, int i, int j, params object[] parameters)";
     }  
+
+     //todo doc 
+     /*
+      la signature ayant le plus de paramètre, et utilisant params, est la seule considérée dans le switch  {$_ -gt 4} 
+     Pour différencier les deux méthodes on doit préciser un type tableau pour la méthode " ArrayOfParams(this string S, int i, int j, params object[] parameters) "
+     Exemple :
+      's'.ArrayOfParams(1,2,3)
+      's'.ArrayOfParams(1,2,3,4)
+       ETS call --> (this string S, int i, int j, int k, params object[] parameters)
+
+       's'.ArrayOfParams(1,2,@(3))
+       's'.ArrayOfParams(1,2,@(3,4))
+      ETS call --> (this string S, int i, int j, params object[] parameters)
+     */
+    public static string ArrayOfParams(this string S, int i, int j, int k, params object[] parameters)
+    {
+       Console.WriteLine("parameters :");
+       Array.ForEach(parameters, Console.WriteLine);
+       return "(this string S, int i, int j, int k, params object[] parameters)";
+    } 
   }  
 "@
 
